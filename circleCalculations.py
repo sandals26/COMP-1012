92f3f606cdbf982a93006d2d6118405800f6377c
sandals26 <kanick980@gmail.com>
2026-09-11 14:10:57 -0500

    Update circleCalculations.py

--- circleCalculations.py
+++ circleCalculations.py
@@ -1,3 +1,11 @@
+"""DogoodRodneyA1Q1
+COMP 1012 SECTION A01
+INSTRUCTOR [Name of your instructor]
+ASSIGNMENT: A1 Question 1
+AUTHOR Nick Karpenko
+VERSION 2026-Sep-11
+PURPOSE: find the area and circumference of a circle given the radius
+"""
 PI = 3.14
 radius= int(input("what is the radius of the circle?\n"))
 area = PI * radius ** 2
