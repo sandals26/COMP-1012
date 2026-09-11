692b111b7e32cdb9c8165f2277d4e7eecfa4b622
sandals26 <kanick980@gmail.com>
2026-09-11 14:06:52 -0500

    Create circleCalculations.py

---
+++ circleCalculations.py
@@ -0,0 +1,5 @@
+PI = 3.14
+radius= int(input("what is the radius of the circle?\n"))
+area = PI * radius ** 2
+circumference = 2 * PI * radius
+print(f"The area of the circle is {area}, and the circumference is {circumference}")
