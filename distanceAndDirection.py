9f79e84ebc0b0867d830cbe18666e0087b603b0d
sandals26 <kanick980@gmail.com>
2026-09-14 13:38:01 -0500

    Update distanceAndDirection.py

--- distanceAndDirection.py
+++ distanceAndDirection.py
@@ -1,3 +1,5 @@
 import math
 x1 = float(input("x1: ")) #co-ordianate relative to origin, in km
 y1 = float(input("y1: ")) #co-ordianate relative to origin, in km
+x2 = float(input("x2: ")) #co-ordianate relative to origin, in km
+y2 = float(input("y2: ")) #co-ordianate relative to origin, in km
