354832cd7446745235bee02356f6ba24ddefd732
sandals26 <kanick980@gmail.com>
2026-09-14 14:13:48 -0500

    Update distanceAndDirection.py

--- distanceAndDirection.py
+++ distanceAndDirection.py
@@ -6,6 +6,7 @@ y2 = float(input("y2: ")) #co-ordianate relative to origin, in km
 distance = ((x2 - x1)**2 + (y2 - y1)**2)**(1/2)
 angle = math.atan2((y2-y1),(x2-x1)) * 180/math.pi #Angle made from the x axis to hypotenuse made by the x and y co-ordinates
 #arctan x = pi/2 - arctan(1/x)
-if()
+if (y2-y1)/(x2-x1) >= 1:
+    print("sumshit")
 taylorSeries = 0
 print("You will walk a distance of {:.3f}km, in {:.3f} hours at a speed of 4km per hour, at an angle of {:.2f} degrees".format(distance, distance/4, angle))
