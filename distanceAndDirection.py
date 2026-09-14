7ce7488cd023b42c4a7eeeee6b17e57c82ea4fc5
sandals26 <kanick980@gmail.com>
2026-09-14 13:47:33 -0500

    Update distanceAndDirection.py

--- distanceAndDirection.py
+++ distanceAndDirection.py
@@ -3,3 +3,5 @@ x1 = float(input("x1: ")) #co-ordianate relative to origin, in km
 y1 = float(input("y1: ")) #co-ordianate relative to origin, in km
 x2 = float(input("x2: ")) #co-ordianate relative to origin, in km
 y2 = float(input("y2: ")) #co-ordianate relative to origin, in km
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print("You will walk a distance of {:.3f}km, in {:.3f} hours at a speed of 4km per hour".format(distance, distance/4))
