3f54a8e15f11dfacad18af2b27d2bbe6dfb8def9
sandals26 <kanick980@gmail.com>
2026-09-15 09:04:42 -0500

    Some stuff

---
+++ rainfallActivity.py
@@ -0,0 +1,2 @@
+riverArea = 2 #in km squred
+surroundingArea = 100 #in km squared
--- shippingContainers.py
+++ shippingContainers.py
@@ -12,7 +12,7 @@ hugeContainers = 0
 largeContainers = 0
 mediumContainers = 0
 smallContainers = 0
-while numWidgets > 0:
+"""while numWidgets > 0:
     if numWidgets >= 50:
         numWidgets -= 50
         hugeContainers += 1
@@ -24,5 +24,13 @@ while numWidgets > 0:
         mediumContainers += 1
     else:
         numWidgets -= 1
-        smallContainers += 1
+        smallContainers += 1"""
+hugeContainers = numWidgets//50 #50 is the amount of widgets a "huge container" could hold
+numWidgets -= numWidgets//50 * 50
+largeContainers = numWidgets//20 #20 is the amount of widgets a "large container" could hold
+numWidgets -= numWidgets//20 * 20
+mediumContainers = numWidgets//5#5 is the amount of widgets a "medium container" could hold
+numWidgets -= numWidgets//5 * 5
+smallContainers = numWidgets//1#1 is the amount of widgets a "small container" could hold
+numWidgets -= numWidgets//1 
 print("To ship {} widgets, use {} huge containers, {} large containers, {} medium containers, and {} small containers".format(totalNumWidgets, hugeContainers, largeContainers, mediumContainers, smallContainers))
