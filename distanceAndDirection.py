94bc6e5cac6f09ac2677a95db3878914e9fb8454
sandals26 <kanick980@gmail.com>
2026-09-14 20:35:01 -0500

    Update distanceAndDirection.py

--- distanceAndDirection.py
+++ distanceAndDirection.py
@@ -1,3 +1,11 @@
+"""DogoodRodneyA1Q1
+COMP 1012 SECTION A01
+INSTRUCTOR Saulo Q. Dos Santos
+ASSIGNMENT: A01 Week 01 Activity 1
+AUTHOR Nick Karpenko
+VERSION 2026-Sep-11
+PURPOSE: find the length, and angle of a vector formed from points (x1,y1) and (x2,y2) in km and how long it would take to walk it if walking at 4 km/h
+"""
 import math
 x1 = float(input("x1: ")) #co-ordianate relative to origin, in km
 y1 = float(input("y1: ")) #co-ordianate relative to origin, in km
