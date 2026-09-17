d928590e0d1c4d1f2191fefb5cbd4843b2894655
sandals26 <kanick980@gmail.com>
2026-09-17 08:55:43 -0500

    Update rainfallActivity.py

--- rainfallActivity.py
+++ rainfallActivity.py
@@ -1,7 +1,7 @@
-lakeArea = 2 #in km squred
-surroundingArea = 100 #in km squared
+LAKE_AREA = 2 #in km squred
+COLLECTION_AREA = 100 #in km squared
 CHANGE_IN_LAKE = 1 # in meters
 #surroundingArea * rainfall / lake area = change in lake height
-rainfall = CHANGE_IN_LAKE*lakeArea*(10**3)/surroundingArea #rainfall in mm
+rainfall = CHANGE_IN_LAKE*LAKE_AREA*(10**3)/COLLECTION_AREA #rainfall in mm
 print("To raise the lake height by {} meter, it would have to rain {:.1f} millimeters".format(CHANGE_IN_LAKE, rainfall))
 
